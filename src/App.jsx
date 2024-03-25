e1ec92a647484abb439fcc3d69952c8d0082fecf
Lama1510 <227168@ppu.edu.ps>
2024-03-26 01:33:55 +0200

    task1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ let name = " yaffa ayaydah "
+ let age ="20 years "
+  return (
+    <div>
+      <h1> hello {name} </h1>
+      <h1> Your Age is {age }</h1>
+    </div>
+  )
+}
+
+export default App
